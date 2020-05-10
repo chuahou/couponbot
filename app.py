658784eae7fdad0f40f9f21aa14ee9604b360514
Chua Hou <human+github@chuahou.dev>
2020-05-10 22:37:25 +0800

    Add database

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import Regexp
+import psycopg2
 import os
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(32)
 Bootstrap(app)
 
-name = os.getenv("NAME", "Invalid Name")
-recipient = os.getenv("RECIPIENT", "Invalid Recipient")
+name = os.environ["NAME"]
+recipient = os.environ["RECIPIENT"]
+conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+cursor = conn.cursor()
 
 class CouponForm(FlaskForm):
-    code = StringField("Username", validators=[DataRequired()])
+    code = StringField("Coupon code", validators=[Regexp("[A-Z0-9]{7}")])
     submit = SubmitField("Use Code")
 
 @app.route("/", methods=['GET', 'POST'])
@@ -22,13 +25,24 @@ class CouponForm(FlaskForm):
 def index():
     form = CouponForm()
     if form.validate_on_submit():
-        flash(f"Code submitted: {form.code.data}")
-        return redirect("/submit")
+        return redirect(url_for("submit", code=form.code.data))
     return render_template("index.html", name=name,
             form=form, invalid=(len(form.code.errors) > 0))
 
 @app.route("/submit")
 def submit():
+    # get records matching our code
+    code = request.args.get("code")
+    cursor.execute("SELECT offer, used FROM codes WHERE code = '" + code + "'")
+    results = cursor.fetchall()
+
+    # print error if invalid code or used code
+    if (len(results) == 0):
+        return "Invalid code"
+    elif (results[0][1]): # used
+        return "Code already used"
+
+    # return template
     return render_template("submit.html", name=name, recipient=recipient,
-            code="ASKDJ", offer="Offer")
+            code=code, offer=results[0][0])
 
