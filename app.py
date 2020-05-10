e59007c300dd254a25d474711041b04e7b561455
Chua Hou <human+github@chuahou.dev>
2020-05-10 21:02:46 +0800

    Add basic Flask-WTF form

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import os
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.urandom(32)
+
+class Form(FlaskForm):
+    code = StringField("Username", validators=[DataRequired()])
+    submit = SubmitField("Use Code")
 
 @app.route("/")
 def index():
-    return render_template("base.html", name=os.getenv("NAME", "Invalid Name"))
+    return render_template("base.html", name=os.getenv("NAME", "Invalid Name"),
+            form=Form())
