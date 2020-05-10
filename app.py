143658d68c20d248e1e6c86b3e13fb96981faeab
Chua Hou <human+github@chuahou.dev>
2020-05-10 23:02:47 +0800

    Refactor and reorganise app

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
+
+# flask imports and extensions
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Regexp
-import psycopg2
-import os
 
+# PostgreSQL
+import psycopg2
+
+# standard imports
+import os # environ
+
+# main app setup
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(32)
 Bootstrap(app)
 
-name = os.environ["NAME"]
-recipient = os.environ["RECIPIENT"]
-conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
-cursor = conn.cursor()
+# get environment variables
+NAME = os.environ["NAME"]
+RECIPIENT = os.environ["RECIPIENT"]
+DATABASE_URL = os.environ["DATABASE_URL"]
 
+# main coupon submission form
+# only accepts uppercase alpha numeric input of length 7
 class CouponForm(FlaskForm):
     code = StringField("Coupon code", validators=[Regexp("[A-Z0-9]{7}")])
     submit = SubmitField("Use Code")
@@ -26,30 +35,47 @@ def index():
     form = CouponForm()
     if form.validate_on_submit():
         return redirect(url_for("submit", code=form.code.data))
-    return render_template("index.html", name=name,
+    return render_template("index.html", name=NAME,
             form=form, invalid=(len(form.code.errors) > 0))
 
 @app.route("/submit")
 def submit():
-    # get records matching our code
-    code = request.args.get("code")
-    cursor.execute("SELECT offer, used FROM codes WHERE code = '" + code + "'")
-    results = cursor.fetchall()
-
-    # error if invalid code or used code
-    offer = ""
+    # stores error message
     error = None
-    if (len(results) == 0):
-        error = "Invalid code"
-    elif (results[0][1]): # used
-        error = "Code already used"
-    else: # okay
-        offer = results[0][0] # get offer text
-        cursor.execute( # set offer to used
-                "UPDATE codes SET used = true WHERE code = '" + code + "'")
-        conn.commit()
+
+    # stores retrieved offer
+    offer = ""
+
+    try:
+        # connect to database
+        conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+        cursor = conn.cursor()
+
+        # get records matching our code
+        code = request.args.get("code")
+        cursor.execute(
+                "SELECT offer, used FROM codes WHERE code = '" + code + "'")
+        results = cursor.fetchall()
+
+        # error if invalid code or used code
+        if (len(results) == 0):
+            error = "Invalid code"
+        elif (results[0][1]): # used
+            error = "Code already used"
+        else: # okay
+            offer = results[0][0] # get offer text
+            cursor.execute( # set offer to used
+                    "UPDATE codes SET used = true WHERE code = '" + code + "'")
+            conn.commit()
+    except (Exception, psycopg2.Error) as error:
+        error = "Database connection error"
+    finally:
+        # close connection
+        if (conn):
+            cursor.close()
+            conn.close()
 
     # return template
-    return render_template("submit.html", name=name, recipient=recipient,
+    return render_template("submit.html", name=NAME, recipient=RECIPIENT,
             code=code, offer=offer, error=error, success=(error == None))
 
